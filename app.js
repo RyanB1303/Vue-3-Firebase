081d94017d19aa15dea7c46488d781491d3ec87b
Ryan B <ryan.brilliant.nirwana@gmail.com>
2021-04-25 14:07:49 +0700

    my-solution

--- app.js
+++ app.js
@@ -3,21 +3,41 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
       ],
-    }
+    };
   },
   methods: {
     toggleShowBooks() {
-      this.showBooks =  !this.showBooks
+      this.showBooks = !this.showBooks;
     },
-  }
-})
+    toggleFav(book) {
+      const selectedBook = this.books.find((item) => item.title === book);
+      selectedBook.isFav = !selectedBook.isFav;
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
 
 // Challenge - Add to Favs
 //   - attach a click event to each li tag (for each book)
 //   - when a user clicks an li, toggle the 'isFav' property of that book
+// Challenge Done :)
